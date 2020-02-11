0bf47846f4eb74fcf8195555e2225c757997e7a1
psych0sykes <blakesykes13@gmail.com>
2020-02-10 23:43:26 -0700

    Trying again

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-var PORT = 3000 || process.env.PORT
+var PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
